da0f918dd09ab756919acc9d9dec842894495cc5
Crisiano Dias <cristiano.dias@bluewin.ch>
2023-01-04 15:36:30 +0100

    fixed database creation

--- database/init-scripts/01_init-user-db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-	CREATE USER admin;
-	CREATE DATABASE cifraeasy;
-	GRANT ALL PRIVILEGES ON DATABASE cifraeasy TO admin;
-    
-EOSQL
